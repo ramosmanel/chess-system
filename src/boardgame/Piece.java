b5e4ea8e8b0ba03b6cfa72807a40ce8e55a29467
ramosmanel <emanuelramos20004@gmail.com>
2024-09-02 16:32:42 -0300

    refactor: variables to constants

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,7 +2,7 @@ package boardgame;
 
 public class Piece {
     protected Position position;
-    private Board board;
+    private final Board board;
 
     public Piece(Board board) {
         this.board = board;
