24fc670cddde1b288fdffd73fca8e6a56120834e
ramosmanel <emanuelramos20004@gmail.com>
2024-09-06 15:55:08 -0300

    feat: remove piece that is in position

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,6 +38,15 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) throw new BoardException("Position not on the board");
+        if(piece(position) == null) return null;
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
     private boolean positionExits(int row, int column){
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
